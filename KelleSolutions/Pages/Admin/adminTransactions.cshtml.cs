6f7f37bc5b5c55dbbfac9fc099d9210ceefa71d7
kylemucha <kylemuchaswe@gmail.com>
2025-02-09 08:40:07 -0800

    SCRUM #148 progress

---
+++ KelleSolutions/Pages/Admin/adminTransactions.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KelleSolutions.Pages.Admin
+{
+    public class TransactionsModel : PageModel
+    {
+        public void OnGet()
+        {
+            // This will be where we fetch transaction data from the database
+        }
+    }
+}
