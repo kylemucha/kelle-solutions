b2a215dd4955eee010969365571f251f39f7994a
kylemucha <kylemuchaswe@gmail.com>
2025-02-24 13:29:46 -0800

    SCRUM 285 - Implemented dashboard with adding capabilites for Broker Dasboard

---
+++ KelleSolutions/Pages/Broker/brokerDashboard.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KelleSolutions.Pages.Broker
+{
+    public class brokerDashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+} 
