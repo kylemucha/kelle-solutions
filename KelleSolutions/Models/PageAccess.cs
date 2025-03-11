76fa51191166a73350105a5d134470bdc1a75d4d
Brandon Kmiec <114979976+brandon-kmiec@users.noreply.github.com>
2025-03-10 23:35:55 -0700

    Updated PageAccess
    Updated PageAccess to fix errors when running project

--- KelleSolutions/Models/PageAccess.cs
+++ KelleSolutions/Models/PageAccess.cs
@@ -1,7 +1,17 @@
-﻿namespace KelleSolutions.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KelleSolutions.Models
 {
     public class PageAccess
     {
+        [Key]
+        [Required]
+        public int RoleID { get; set; }
+
+        [MaxLength(6)]
+        [Required]
+        public string RoleName { get; set; } = null!;
         // TODO: dotnet ef migrations add RolePermissionGroupEntity
         // command doesnt work until PageAccess table is implemented
     }
